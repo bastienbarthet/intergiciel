a4cf988920364e1122982e703c860ab94410b6da
bastien.lehmann <bastien.lehmann@e2ef325c-1464-11de-99d6-5fb3f1362b5f>
2009-03-23 14:42:34 +0000

    Class SharedObject avancé

--- etape1/src/SharedObject.java
+++ etape1/src/SharedObject.java
@@ -5,9 +5,10 @@ public class SharedObject implements Serializable, SharedObject_itf {
 	// defition des etats d'un shared object
 	public static final int NL = 0;				// no local read
 	public static final int RLC = 1;			// real lock caches (not taken)
-	public static final int RTL = 3;			// read lock taken
+	public static final int WLC = 2;			// write lock cached
+	public static final int RLT = 3;			// read lock taken
 	public static final int WLT = 4;			// write lock taken
-	public static final int RTL_WLC = 5;		// read lock taken and write lock cached
+	public static final int RLT_WLC = 5;		// read lock taken and write lock cached
 		
 	// variable d'etat du shared object
 	private int lock;
@@ -24,7 +25,7 @@ public class SharedObject implements Serializable, SharedObject_itf {
 		return this.o;
 	}
 	
-	// idantifiant de l'objet
+	// identifiant de l'objet
 	private int id;
 	public int getId() {
 		return this.id;
@@ -39,27 +40,84 @@ public class SharedObject implements Serializable, SharedObject_itf {
 	
 	// invoked by the user program on the client node
 	public void lock_read() {
+		// si RLC, direct RLT
+		if (this.getLock()==RLC) {
+			this.setLock(RLT);
+		}
+		else {
+			// ici il faut appeler lock_read du client, pour que celui ci demande le lock_read au serveur			
+		}
+
 	}
 
 	// invoked by the user program on the client node
 	public void lock_write() {
+		// si RLC, direct RLT
+		if (this.getLock()==WLC) {
+			this.setLock(WLT);
+		}
+		else {
+			// ici il faut appeler lock_write du client, pour que celui ci demande le lock_read au serveur et obtienne si necessaire une nouvelle copie			
+		}
+
 	}
 
 	// invoked by the user program on the client node
 	public synchronized void unlock() {
+		switch (this.getLock()) {
+		case RLT : this.setLock(RLC); break;
+		case WLT : this.setLock(WLC); break;
+		case RLT_WLC : this.setLock(WLC); break;
+		default : break;
+		}
 	}
 
 
 	// callback invoked remotely by the server
 	public synchronized Object reduce_lock() {
-		return null;
+		// permet au serveur de réclamer le passage d'un verrou de l'écriture a la lecture
+		switch (this.getLock()) {
+		
+		case NL : break;
+		case RLC :  break;
+		case WLC : this.setLock(RLC); break;
+		// probleme sur ces 2 ci dessous!! Il faut waiter que l'appli ai fini pour passer en lecteur...
+			// Solution pour WLT : le serveur sait si ce shared object écrit, et va pa lui dire merde
+			// Solution pour RLT : il faut waiter...
+		case RLT : this.setLock(RLC); break;
+		case WLT : this.setLock(WLC); break;
+			// case RLT_WLC : il faut waiter aussi
+		case RLT_WLC : this.setLock(WLC); break;
+		default : break;
+		}
+	return null; // On est sencer renvoyé qq chose?? moi je crois pas..
 	}
 
 	// callback invoked remotely by the server
 	public synchronized void invalidate_reader() {
+		// 2 cas : RLT ou RLC
+		switch (this.getLock()) {
+		
+		//case RLT : il faut waiter
+		case RLT : break;
+		//cas RLC, on invalide
+		case RLC :  this.setLock(NL); break;
+		case RLT_WLC : break; // il faut waiter...
+		case WLC :  this.setLock(NL); break;
+		// case WLT : pas possible, le serveur va pas invalider l'écrivain en cours
+		default : break;
+		}
+		
 	}
 
 	public synchronized Object invalidate_writer() {
-		return null;
+	switch (this.getLock()) {
+		
+		//Le serveur ne peut invalider que des WLC selon moi..
+		case WLC :  this.setLock(NL); break;
+		default : break;
 	}
+		return null;
+}
+	
 }
