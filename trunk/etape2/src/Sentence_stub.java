07175133df61cd2cf0cbef8e3d89b0508be18b59
bastien.barthet <bastien.barthet@e2ef325c-1464-11de-99d6-5fb3f1362b5f>
2009-04-27 12:11:39 +0000

    attentyion on y va

--- trunk/etape2/src/Sentence_stub.java
+++ trunk/etape2/src/Sentence_stub.java
@@ -1,11 +1,11 @@
 public class Sentence_stub extends SharedObject implements Sentence_itf, java.io.Serializable {
 	
 	public void write(String text) {
-		Sentence s = (Sentence)obj;
+		Sentence s = (Sentence)o;
 		s.write(text);
 	}
 	public String read() {
-		Sentence s = (Sentence)obj;
+		Sentence s = (Sentence)o;
 		return s.read();	
 	}
 	
