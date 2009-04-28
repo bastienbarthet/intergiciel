32e80c7e5aba689b4ceb4d8587c3197d87cfe103
bastien.lehmann <bastien.lehmann@e2ef325c-1464-11de-99d6-5fb3f1362b5f>
2009-04-28 19:24:08 +0000

    le stubgénérateur ma lair bien

--- trunk/etape2/src/GenerateurDeStub.java
+++ trunk/etape2/src/GenerateurDeStub.java
@@ -26,16 +26,19 @@ public class GenerateurDeStub {
 		code += "	private static final long serialVersionUID = 3173827029004946005L; \n\n";
 		
 		// Le constructeur
-		code += "	public " + nomClasse + "_stub (int id, Object o){ \n		super(id, o) \n	} \n \n";
+		code += "	public " + nomClasse + "_stub(int id, Object o){ \n		super(id, o); \n	} \n \n";
 		
-		//tableau des mÃ©thodes de la classe
+		//tableau des méthodes de la classe
 		Method[] methodes = classe.getDeclaredMethods();
 		
+		// le code de chaque methode public, pas hérité
 		for (int i = 0; i < methodes.length; i++) {
-	            Method meth = methodes[i];
+			Method meth = methodes[i];
+			boolean estPublic = Modifier.isPublic(meth.getModifiers());    
+			if (estPublic) {
+			    String typeRetour = meth.getReturnType().getName();
 	            String nom = meth.getName();
-	            boolean estPublic = Modifier.isPublic(meth.getModifiers());
-	            String typeRetour = meth.getReturnType().getName();
+	            
 	            Class[] params = meth.getParameterTypes();
 	            //params[j].getName();
 	            
@@ -43,19 +46,34 @@ public class GenerateurDeStub {
 
 	         for(int j=0; j<params.length; j++ ){
 	        	 String typeAttribut = params[j].getSimpleName();
-	        	 code += typeAttribut +" "+ params[j]+", ";	    	 
+	        	 code += typeAttribut +" argument"+j;
+	        	 if (j!=params.length-1) code += ", ";
 	        	 }
 	         
-	         code += "){ \n\n";
+	         code += "){ \n";
       //corp de la mÃ©thode
+	         String valeurRetour = meth.getReturnType().toString();
 	         
-	         
-	// fin de la mÃ©thode 	         
-	code +="\n	}\n\n";
+	      // on récupère l'objet partagé obj
+				code += "		" + nomClasse + " o = (" + nomClasse + ") obj ; \n";
+	      // et on appelle la méthode avec les bons paramètres
+				if (!typeRetour.equals("void")){
+					code += "		return ";
+				}
+
+				code += "	o." + meth.getName() + "(";
+				for (int k = 0; k < meth.getParameterTypes().length; k++){
+					code += "argument"+k;
+					if (k < meth.getParameterTypes().length-1){
+						code += ", ";
+					}
+				}
+				code += "); \n	}\n\n";
+
+		}
 		}
 
-		
-		
+		// fin du fichier
 		code +="\n\n}\n";
 
 		try {		
