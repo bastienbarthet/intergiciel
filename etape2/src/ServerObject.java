03949e9383a628e3c7987351e395b73a1f1858dd
bastien.barthet <bastien.barthet@gmail.com>
2009-05-03 16:08:40 +0000

    javadocé

--- etape2/src/ServerObject.java
+++ etape2/src/ServerObject.java
@@ -3,94 +3,127 @@ import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-
+/**
+ * 
+ * @author Bastiens Barthet&Lehmann
+ * Classe qui stock l'objet, les lecteurs et l'écrivain, coté serveur
+ *
+ */
 public class ServerObject implements Serializable{
 
 	private static final long serialVersionUID = -7396577889952667013L;
-	
-	// liste des clients
+
+	/**
+	 *  liste des clients
+	 */
 	public ArrayList<Client_itf> listeDesLecteurs;
+
+	/**
+	 * 
+	 * @return la liste des clients
+	 */
 	public ArrayList<Client_itf> getListe() {
 		return this.listeDesLecteurs;
 	}
-	
+
 	private int ID;
+
+	/**
+	 * 
+	 * @param nouvelID
+	 */
 	public void setID(int nouvelID) {
 		this.ID = nouvelID;
 	}
+
+	/**
+	 * 
+	 * @return L'ID
+	 */
 	public int getID() {
 		return this.ID;
 	}
-	
+
 	private Client_itf client_ecrivain;
-	
-	// objet sur lequel pointe le server object
+
 	private Object o;
+
+
+	/**
+	 * 
+	 * @return l'objet
+	 */
 	public Object getObject() {
 		return this.o;
 	}
-	
-	// nom de l'objet
+
 	private String name;
+
+	/**
+	 * 
+	 * @return le nom de l'objet
+	 */
 	public String getName() {
 		return this.name;
 	}
-	
-	// constructeur
+
+	/**
+	 * 
+	 * @param id l'ID
+	 * @param o l'objet
+	 */
 	public ServerObject (int id, Object o){
 		this.ID = id;
 		this.listeDesLecteurs = new ArrayList<Client_itf>();
 		this.client_ecrivain = null;
 		this.o = o;
 	}
-	
+
+	/**
+	 * accede à la requete d'un client sur les droits de lecture
+	 * @param client demandeur
+	 * @return l'objet
+	 * @throws RemoteException
+	 */
 	public synchronized Object lock_read(Client_itf client) throws RemoteException {
 
-		// ON TE DONNE LE DROIT DE LECTURE
 		this.listeDesLecteurs.add(client);
-		
+
 		if (this.client_ecrivain!=null) {
-			// on invalide l'ecrivain
 			this.o = client_ecrivain.reduce_lock(this.ID);
 			this.listeDesLecteurs.add(client_ecrivain);
 			this.client_ecrivain=null;
 		}
-
-		
-		
-		// va renvoyer un truc de ce genre
 		//System.out.println("fin de lock_read : " );
 		return this.getObject();
-		
+
 	}
-	
+
+	/**
+	 * accede à la requete d'un client sur les droits d'écriture
+	 * @param client demandeur
+	 * @return l'objet
+	 * @throws RemoteException
+	 */
 	public synchronized Object lock_write(Client_itf client) throws RemoteException {
-		
-		//System.out.println("taille liste lecteur" + this.listeDesLecteurs.size());
+
 		this.listeDesLecteurs.remove(client);
-		
+
 		Iterator<Client_itf> it = this.listeDesLecteurs.iterator() ;
 		while ( it.hasNext() ) {
 			it.next().invalidate_reader(this.ID);
 		}
-		//for (int i=0 ; i<( this.listeDesLecteurs.size() ) ; i++) {
-		//	this.listeDesLecteurs.get(i).invalidate_reader(this.ID);
-		//}
-		
-		
+
 		this.listeDesLecteurs.clear();
-		
+
 		if (this.client_ecrivain!=null) {
-			// on invalide l'ecrivain
 			this.o = client_ecrivain.invalidate_writer(this.ID );
 		}
-		
-		//  ON TE DONNE LE DROIT D'ECRITURE
+
 		this.client_ecrivain=client;
-		
-		// va renvoyer un truc de ce genre
-		//System.out.println("fin de lock_write : ");
+
+		//	System.out.println("fin de lock_write : ");
 		return this.getObject();
 	}
-	
+
 }
--- etape2/src/SharedObject.java
+++ etape2/src/SharedObject.java
@@ -1,10 +1,14 @@
 import java.io.*;
 import java.rmi.RemoteException;
-
+	/**
+	 * 
+	 * @author Bastiens Barthet&Lehmann
+	 *	Classe SharedObject, qui stock la donnée transmise.
+	 */
 public class SharedObject implements Serializable, SharedObject_itf {
-	
+
 	private static final long serialVersionUID = -299256425145734063L;
-	
+
 	// defition des etats d'un shared object
 	public static final int NL = 0;				// no local read
 	public static final int RLC = 1;			// real lock caches (not taken)
@@ -12,66 +16,83 @@ public class SharedObject implements Serializable, SharedObject_itf {
 	public static final int RLT = 3;			// read lock taken
 	public static final int WLT = 4;			// write lock taken
 	public static final int RLT_WLC = 5;		// read lock taken and write lock cached
-		
+
 	// variable d'etat du shared object
 	private int lock;
+
+	/**
+	 * 
+	 * @return lock, l'etat courant du verrou
+	 */
 	public int getLock() {
 		return this.lock;
 	}
+	
+	
+	/**
+	 * 
+	 * @param newLock le nouveau lock
+	 */
 	public void setLock(int newLock) {
 		this.lock = newLock;
 	}
 
-	// variable de nom du shared object
-//	private String name;
-//	public String getName() {
-//		return this.name;
-//	}
-//	public void setName(String newName) {
-//		this.name = newName;
-//	}
-	
-	
-	// objet sur lequel pointe le shared object
+
+	/**
+	 * L'object stocké
+	 */
 	public Object obj;
-	//public Object getObject() {
-	//	return this.o;
-	//}
+
 	
-	// identifiant de l'objet
+	/**
+	 * L'ID de l'objet partagé
+	 */
 	private int id;
+	
+	/**
+	 * 
+	 * @return l'ID
+	 */
 	public int getId() {
 		return this.id;
 	}
-	
-	// constructeur
+
+	/**Constructeur
+	 * 
+	 * @param id l'ID de l'objet
+	 * @param _o L'object
+	 */
 	public SharedObject (int id, Object _o){
 		this.id = id;
 		this.lock = NL;
 		this.obj = _o;
 	}
-	
-	// invoked by the user program on the client node
+
+
+	/**
+	 * Demande (et obtient) les droits en lecture
+	 */
 	public void lock_read() {
 		synchronized (this) {
 			switch (this.lock) {
-			// si RLC, direct RLT
 			case RLC :	this.lock = RLT; break; 
 			case WLC : 	this.lock = RLT_WLC; break;
 			case NL : try {
-						this.obj = Client.lock_read(this.getId());
-						this.lock=RLT;
-					} catch (RemoteException e) {
-						e.printStackTrace();
-					}
-					break;
+				this.obj = Client.lock_read(this.getId());
+				this.lock=RLT;
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
+			break;
 			default : System.err.println("cas pas possible de verrou ds lock_read"); break;
 			}
 		}
 		//System.out.println( "fin du lock_read : " +this.lock);
 	}
 
-	// invoked by the user program on the client node
+	/**
+	 * Demande (et obtient) les droits en ecriture
+	 */
 	public void lock_write() {
 		boolean maj = false;
 		synchronized (this) {
@@ -89,24 +110,30 @@ public class SharedObject implements Serializable, SharedObject_itf {
 				e.printStackTrace();
 			}
 		}
-		
+
 		//System.out.println( "fin du lock_write : " +this.lock);
 	}
 
-	// invoked by the user program on the client node
+	/**
+	 * Methode de relache du verrou
+	 */
 	public synchronized void unlock() {
 		switch (this.lock) {
-			case RLT : this.lock = RLC; break;
-			case WLT : this.lock = WLC; break;
-			case RLT_WLC : this.lock = WLC; break;
-			default : break;
+		case RLT : this.lock = RLC; break;
+		case WLT : this.lock = WLC; break;
+		case RLT_WLC : this.lock = WLC; break;
+		default : break;
 		}
 		notify();
-		//System.out.println( "fin du unlock : " +this.lock);
+		//	System.out.println( "fin du unlock : " +this.lock);
 	}
 
 
-	// callback invoked remotely by the server
+	/**
+	 * Reduit les droits de l'écrivain en simple lecteur
+	 * @return la derniere version de l'object
+	 * @throws InterruptedException
+	 */
 	public synchronized Object reduce_lock() throws InterruptedException {
 		// permet au serveur de rÃ©clamer le passage d'un verrou de l'Ã©criture a la lecture
 		while(this.lock!=RLT_WLC && this.lock!=WLC) {
@@ -114,32 +141,40 @@ public class SharedObject implements Serializable, SharedObject_itf {
 				wait();
 			} catch (InterruptedException e) {}
 		}
-		
+
 		switch (this.lock) {
-			case WLC : this.lock = RLC; break;
-			case RLT_WLC : this.lock = RLT; break;
-			default : break;
+		case WLC : this.lock = RLC; break;
+		case RLT_WLC : this.lock = RLT; break;
+		default : break;
 		}
 		//System.out.println( "fin du reduce_lock : " +this.lock);
 		return this.obj;
 	}
 
-	
-	// callback invoked remotely by the server
+
+	/**	Invalide les droits d'écriture
+	 * 
+	 * @throws InterruptedException
+	 */
 	public synchronized void invalidate_reader() throws InterruptedException {
 		while(this.lock!=WLT && this.lock!=RLC){
 			try {
 				wait();
 			} catch (InterruptedException e) {}
 		}
-		
+
 		switch (this.lock) {
-			case RLC :  this.lock = NL; break;
-			default : break;
+		case RLC :  this.lock = NL; break;
+		default : break;
 		}
 		//System.out.println( "fin du invalidate_reader : " +this.lock);
 	}
 
+	/**
+	 * Invalide les droits en écriture
+	 * @return la derniere version de l'objet
+	 * @throws InterruptedException
+	 */
 	public synchronized Object invalidate_writer() throws InterruptedException {
 
 		while(this.lock!=WLC) {
@@ -147,11 +182,11 @@ public class SharedObject implements Serializable, SharedObject_itf {
 				wait();
 			} catch (InterruptedException e) {}
 		}
-		
+
 		this.lock = NL;
-		
+
 		//System.out.println( "fin du invalidate_writer : " +this.lock);
 		return this.obj;	
 	}
-	
+
 }
